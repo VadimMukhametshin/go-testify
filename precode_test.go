672a357579663ffafdad9c79adbd4017c9b2c982
Vadim Mukhametshin <da-vadik@yandex.ru>
2024-01-31 22:55:59 +0300

    second iteration

--- precode_test.go
+++ precode_test.go
@@ -8,23 +8,27 @@ import (
 	"testing"
 )
 
-func TestMainHandlerWhenOk(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
-
+func NewRec(req *http.Request, handler http.HandlerFunc) *httptest.ResponseRecorder {
 	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
+	handler = http.HandlerFunc(handler)
 	handler.ServeHTTP(responseRecorder, req)
+
+	return responseRecorder
+}
+
+func TestMainHandlerWhenOk(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/cafe?count=2&city=moscow", nil)
+
+	responseRecorder := NewRec(req, mainHandle)
 
 	assert.Equal(t, responseRecorder.Code, http.StatusOK)
 	assert.NotEmpty(t, responseRecorder.Body)
 }
 
 func TestMainHandlerWhenWrongCity(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=3&city=wocsom", nil)
+	req := httptest.NewRequest(http.MethodGet, "/cafe?count=3&city=wocsom", nil)
 
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
+	responseRecorder := NewRec(req, mainHandle)
 
 	expected := `wrong city value`
 
@@ -34,11 +38,9 @@ func TestMainHandlerWhenWrongCity(t *testing.T) {
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+	req := httptest.NewRequest(http.MethodGet, "/cafe?count=10&city=moscow", nil)
 
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
+	responseRecorder := NewRec(req, mainHandle)
 
 	resBody := strings.Split(responseRecorder.Body.String(), ",")
 
